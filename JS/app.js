78f45b79a8e2c57c28d3b9a66499a31de48aaae9
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-02-17 01:28:10 +0600

    got income and cost value

--- JS/app.js
+++ JS/app.js
@@ -1,18 +1,47 @@
 function income() {
-    const totalIncome = document.getElementById('total-income').value;
+    const totalInput = document.getElementById('total-income');
+    const totalIncome = totalInput.value;
     const total = parseInt(totalIncome);
     if (total > 0) {
+
+
         return total;
+
     } else {
         alert('Please enter a valid number');
     }
+    totalInput.value = '';
+
 
 
 }
 
 function expenses() {
+    const foodInput = document.getElementById('food-input');
+    const foodCost = foodInput.value;
+    const restInput = document.getElementById('rest-input');
+    const restCost = restInput.value;
+    const clothInput = document.getElementById('cloth-input');
+    const clothCost = clothInput.value;
+    const costOfFood = parseInt(foodCost);
+    const costOfRest = parseInt(restCost);
+    const costOfCloth = parseInt(clothCost);
+
+    const totalExpenses = costOfFood + costOfRest + costOfCloth;
+
+
+    if (costOfFood > 0 && costOfRest > 0 && costOfCloth > 0) {
+        return totalExpenses;
+    } else {
+        alert('Please enter a valid number');
+    }
+    foodInput.value = '';
+    restInput.value = '';
+    clothInput.value = '';
+
 
 }
 document.getElementById('calculate-total').addEventListener('click', function() {
-    income();
+    expenses();
+
 });
