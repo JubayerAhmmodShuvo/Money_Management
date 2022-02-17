32a256b2ebbf94766623a089c7baed910395d15e
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-02-17 15:25:21 +0600

    almost done

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,5 @@
+//function for all input values
+
 function getInputValue(inputId) {
     const inputField = document.getElementById(inputId);
     const inputAmountText = inputField.value;
@@ -8,7 +10,7 @@ function getInputValue(inputId) {
         alert('Please enter a valid amount');
     }
 }
-
+//function for calculating the cost
 function cost() {
 
     const foodInput = getInputValue('food-input');
@@ -19,7 +21,7 @@ function cost() {
 
 
 }
-
+//function for calculating the totalExpense
 function totalExpense() {
     const expense = document.getElementById('total-expense');
     const totalExpense = expense.innerText;
@@ -27,7 +29,7 @@ function totalExpense() {
 
     expense.innerText = total;
 }
-
+//function for calculating the totalBalance
 function totalBalance() {
     const balance = document.getElementById('total-balance');
 
@@ -52,7 +54,7 @@ document.getElementById('calculate-total').addEventListener('click', function() 
         alert('Your balance is insufficient, Please enter value correclty otherwise you will be in debt');
     }
 });
-
+//function for calculating the saving amount
 function save() {
     const totalAmount = getInputValue('total-income');
     const totalPercent = getInputValue('save-input');
@@ -60,18 +62,27 @@ function save() {
     const total = beforeTotal / 100;
     return total;
 }
-
+//function for calculating the saving amount
 function savingAmount() {
     const saveAmount = document.getElementById('save-amount');
-    const saveAmountText = saveAmount.innerText;
-    const total = parseFloat(saveAmountText) + save();
+    const totalBalance = document.getElementById('total-balance').innerText;
+    const balance = parseFloat(totalBalance);
 
-    saveAmount.innerText = total;
-    return total;
+    const saveAmountText = saveAmount.innerText;
+    const saveAmountConverted = parseFloat(saveAmountText);
+    const total = saveAmountConverted + save();
+    if (total > balance) {
+        alert('You cannot save because saving amount is higher than your balance');
+    } else {
+        saveAmount.innerText = total;
+        return total;
+    }
+    /*  saveAmount.innerText = total;
+     return total; */
 
 
 }
-
+//function for calculating the remaining amount
 function remainingBalance() {
     const totalBalance = document.getElementById('total-balance');
     const savingAmount = document.getElementById('save-amount');
@@ -90,14 +101,10 @@ function remainingBalance() {
 
 
 }
-
+//save click button functionalities
 document.getElementById('save-button').addEventListener('click', function() {
-
 
     savingAmount();
     remainingBalance();
-
-
-
 
 });
